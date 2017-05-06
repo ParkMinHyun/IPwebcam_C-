0de93318f590540b914623930b33c44b9d168fee
박민현 <algusl92@naver.com>
2017-05-06 13:03:15 +0900

    IPWebcam 서버에서 Mjpeg 라이브러리 이용하여 Frame Streaming 하기

---
+++ IPwebcam/IPwebcam/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MjpegProcessor;
+namespace IPwebcam
+{
+    public partial class Form1 : Form
+    {
+        MjpegDecoder mjp;
+        public Form1()
+        {
+            InitializeComponent();
+            mjp = new MjpegDecoder();
+            mjp.FrameReady += mjp_frameReady;
+            mjp.ParseStream(new Uri("http://192.168.0.23:8080/videofeed"));
+        }
+
+
+        void mjp_frameReady(object sender, FrameReadyEventArgs e)
+        {
+            pictureBox1.Image = e.Bitmap;
+
+            Image img = pictureBox1.Image;
+            img.RotateFlip(RotateFlipType.Rotate180FlipX);
+            pictureBox1.Image = img;
+        }
+    }
+}
